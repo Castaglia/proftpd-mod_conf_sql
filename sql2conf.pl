15b3f1a8e141ba672f7dafe80f3b4bd5904e974e
TJ Saunders <tj@castaglia.org>
2016-09-17 12:00:46 -0700

    Update sql2conf to support using the SQLite DBD driver, too.  Needs testing.

--- sql2conf.pl
+++ sql2conf.pl
@@ -1,7 +1,4 @@
 #!/usr/bin/env perl
-# ---------------------------------------------------------------------------
-#  $Id: sql2conf.pl,v 1.3 2003/07/02 22:16:32 tj Exp tj $
-# ---------------------------------------------------------------------------
 
 use strict;
 
@@ -10,34 +7,45 @@ use File::Basename qw(basename);
 use Getopt::Long;
 
 my $program = basename($0);
-my %opts = (
-
+my $opts = {
   # Default table parameters
+  'ctx-tab' => 'ftpctx',
   'conf-tab' => 'ftpconf',
-  'ctxt-tab' => 'ftpctxt',
   'map-tab' => 'ftpmap',
-);
+};
 
-GetOptions(\%opts, 'conf-tab=s', 'ctxt-tab=s', 'dbdriver=s', 'dbname=s',
+GetOptions($opts, 'conf-tab=s', 'ctx-tab=s', 'dbdriver=s', 'dbname=s',
   'dbpass=s', 'dbserver=s', 'dbuser=s', 'help', 'map-tab=s', 'show-sql',
   'show-tags', 'verbose');
 
-usage() if $opts{'help'};
+usage() if $opts->{help};
 
-die "$program: missing --dbdriver option\n" unless defined($opts{'dbdriver'});
-die "$program: missing --dbname option\n" unless defined($opts{'dbname'});
-die "$program: missing --dbpass option\n" unless defined($opts{'dbpass'});
-die "$program: missing --dbserver option\n" unless defined($opts{'dbserver'});
-die "$program: missing --dbuser option\n" unless defined($opts{'dbuser'});
+die "$program: missing --dbdriver option\n" unless defined($opts->{dbdriver});
+die "$program: missing --dbname option\n" unless defined($opts->{dbname});
 
-my $dbname = "$opts{'database'}\@$opts{'dbserver'}";
+# We need a database handle.
+my ($dbname, $dbkey, $dsn);
 
-# MySQL driver prefers 'database', Postgres likes 'dbname'
-my $dbkey = ($opts{'dbdriver'} =~ /mysql/i) ? 'database' : 'dbname';
-my $dsn = "DBI:$opts{'dbdriver'}:$dbkey=$opts{'dbname'};host=$opts{'dbserver'}";
+# SQLite, unlike other databases, does not require server/user/pass options.
+if ($opts->{dbdriver} =~ /sqlite/i) {
+  $dbname = "$opts->{dbname}";
+  $dbkey = 'dbname';
+  $dsn = "DBI:$opts->{dbdriver}:$dbkey=$opts->{dbname}";
+
+} else {
+  die "$program: missing --dbpass option\n" unless defined($opts->{dbpass});
+  die "$program: missing --dbserver option\n" unless defined($opts->{dbserver});
+  die "$program: missing --dbuser option\n" unless defined($opts->{dbuser});
+
+  $dbname = "$opts->{dbname}\@$opts->{dbserver}";
+
+  # MySQL driver prefers 'database', Postgres likes 'dbname'
+  $dbkey = ($opts->{dbdriver} =~ /mysql/i) ? 'database' : 'dbname';
+  $dsn = "DBI:$opts->{dbdriver}:$dbkey=$opts->{dbname};host=$opts->{dbserver}";
+}
 
 my $dbh;
-unless ($dbh = DBI->connect($dsn, $opts{'dbuser'}, $opts{'dbpass'})) {
+unless ($dbh = DBI->connect($dsn, $opts->{dbuser}, $opts->{dbpass})) {
   die "$program: unable to connect to $dbname: $DBI::errstr\n";
 }
 
@@ -45,7 +53,7 @@ unless ($dbh = DBI->connect($dsn, $opts{'dbuser'}, $opts{'dbpass'})) {
 # find all the contexts for which this context is the parent.  For each
 # of those contexts, find all directives, etc....
 
-my $conf = get_ctxt();
+my $conf = get_ctx();
 
 show_conf($conf);
 
@@ -64,11 +72,10 @@ sub dbi_prep_sql {
   return $sth;
 }
 
-# ---------------------------------------------------------------------------
 sub dbi_exec_sql {
   my ($sql, $sth) = @_;
 
-  print "$program: executing: $sql\n" if $opts{'show-sql'};
+  print "$program: executing: $sql\n" if $opts->{'show-sql'};
 
   unless ($sth->execute()) {
     warn "$program: error executing '$sql', $DBI::errstr\n";
@@ -78,79 +85,75 @@ sub dbi_exec_sql {
   return 1;
 }
 
-# ---------------------------------------------------------------------------
 sub dbi_free_sql {
   my ($sth) = @_;
   $sth->finish();
 }
 
-# ---------------------------------------------------------------------------
-sub get_ctxt {
+sub get_ctx {
   my ($id) = @_;
-  my $ctxt;
+  my $ctx;
 
   # If no context ID is given, look up the default/"server config" context.
   # It will be the one that has no parent (i.e. parent_id is NULL).
 
-  my $ctxt_tab = $opts{'ctxt-tab'};
+  my $ctx_tab = $opts->{'ctx-tab'};
 
   unless ($id) {
-    my $sql = "SELECT id FROM $ctxt_tab WHERE parent_id IS NULL";
+    my $sql = "SELECT id FROM $ctx_tab WHERE parent_id IS NULL";
     my $sth = dbi_prep_sql($sql);
     dbi_exec_sql($sql, $sth);
     $id = ($sth->fetchrow_array())[0];
     dbi_free_sql($sth);
   }
 
-  my $sql = "SELECT type, info FROM $ctxt_tab WHERE id = $id";
+  my $sql = "SELECT type, value FROM $ctx_tab WHERE id = $id";
   my $sth = dbi_prep_sql($sql);
   dbi_exec_sql($sql, $sth);
 
-  $ctxt->{'id'} = $id;
-  ($ctxt->{'key'}, $ctxt->{'value'}) = ($sth->fetchrow_array())[0, 1];
+  $ctx->{id} = $id;
+  ($ctx->{key}, $ctx->{value}) = ($sth->fetchrow_array())[0, 1];
   dbi_free_sql($sth);
 
-  $ctxt->{'directives'} = get_ctxt_directives($id);
+  $ctx->{directives} = get_ctx_directives($id);
 
-  my $ctxts = get_ctxt_ctxts($id);
+  my $ctxs = get_ctx_ctxs($id);
 
-  foreach $id (@{ $ctxts }) {
-    push(@{ $ctxt->{'contexts'} }, get_ctxt($id));
+  foreach $id (@$ctxs) {
+    push(@{ $ctx->{contexts} }, get_ctx($id));
   }
 
-  return $ctxt;
+  return $ctx;
 }
 
-# ---------------------------------------------------------------------------
-sub get_ctxt_ctxts {
-  my ($ctxt_id) = @_;
+sub get_ctx_ctxs {
+  my ($ctx_id) = @_;
 
-  my $ctxt_tab = $opts{'ctxt-tab'};
-  my $sql = "SELECT id FROM $ctxt_tab WHERE parent_id = $ctxt_id";
+  my $ctx_tab = $opts->{'ctx-tab'};
+  my $sql = "SELECT id FROM $ctx_tab WHERE parent_id = $ctx_id";
 
   my $sth = dbi_prep_sql($sql);
   dbi_exec_sql($sql, $sth);
 
-  my $ctxts;
+  my $ctxs;
   while (my @row = $sth->fetchrow_array()) {
     my ($id) = @row;
-    push(@{ $ctxts }, $id);
+    push(@$ctxs, $id);
   }
 
   dbi_free_sql($sth);
-  return $ctxts;
+  return $ctxs;
 }
 
-# ---------------------------------------------------------------------------
-sub get_ctxt_directives {
-  my ($ctxt_id) = @_;
+sub get_ctx_directives {
+  my ($ctx_id) = @_;
 
-  my $dir_tab = $opts{'conf-tab'};
-  my $map_tab = $opts{'map-tab'};
+  my $dir_tab = $opts->{'conf-tab'};
+  my $map_tab = $opts->{'map-tab'};
 
-  my $sql = "SELECT id, type, info FROM $dir_tab INNER JOIN $map_tab " .
+  my $sql = "SELECT id, type, value FROM $dir_tab INNER JOIN $map_tab " .
             " ON $dir_tab.id = $map_tab.conf_id" .
-            " WHERE $map_tab.ctxt_id = $ctxt_id";
+            " WHERE $map_tab.ctx_id = $ctx_id";
 
   my $sth = dbi_prep_sql($sql);
   dbi_exec_sql($sql, $sth);
@@ -158,59 +161,55 @@ sub get_ctxt_directives {
   my $directives;
   while (my @row = $sth->fetchrow_array()) {
     my ($id, $key, $value) = @row;
-    push(@{ $directives }, { 'id' => $id, 'key' => $key, 'value' => $value });
+    push(@$directives, { id => $id, key => $key, value => $value });
   }
 
   dbi_free_sql($sth);
   return $directives;
 }
 
-# ---------------------------------------------------------------------------
 sub show_conf {
-  my ($ctxt) = @_;
+  my ($ctx) = @_;
 
-  show_directives($ctxt->{'directives'}, 0);
-  show_ctxts($ctxt->{'contexts'}, 0);
+  show_directives($ctx->{directives}, 0);
+  show_ctxs($ctx->{contexts}, 0);
 }
 
-# ---------------------------------------------------------------------------
-sub show_ctxts {
-  my ($ctxts, $indent) = @_;
+sub show_ctxs {
+  my ($ctxs, $indent) = @_;
 
-  foreach my $ctxt (@{ $ctxts }) {
+  foreach my $ctx (@$ctxs) {
     print STDOUT "\n";
-    print STDOUT ' ' x $indent, "<$ctxt->{'key'}",
-      $ctxt->{'value'} ? " $ctxt->{'value'}" : '', ">";
+    print STDOUT ' ' x $indent, "<$ctx->{key}",
+      $ctx->{value} ? " $ctx->{value}" : '', ">";
 
-    if ($opts{'show-tags'}) {
-      print STDOUT " ($opts{'ctxt-tab'}, id $ctxt->{'id'})";
+    if ($opts->{'show-tags'}) {
+      print STDOUT " ($opts->{'ctx-tab'}, id $ctx->{'id'})";
     }
 
     print STDOUT "\n";
 
-    show_directives($ctxt->{'directives'}, $indent + 2);
-    show_ctxts($ctxt->{'contexts'}, $indent + 2);
+    show_directives($ctx->{directives}, $indent + 2);
+    show_ctxs($ctx->{contexts}, $indent + 2);
 
-    print STDOUT ' ' x $indent, "</$ctxt->{'key'}>\n";
+    print STDOUT ' ' x $indent, "</$ctx->{key}>\n";
   }
 }
 
-# ---------------------------------------------------------------------------
 sub show_directives {
   my ($directives, $indent) = @_;
 
-  foreach my $directive (@{ $directives }) {
-    print STDOUT ' ' x $indent, "$directive->{'key'} $directive->{'value'}";
+  foreach my $directive (@$directives) {
+    print STDOUT ' ' x $indent, "$directive->{key} $directive->{value}";
 
-    if ($opts{'show-tags'}) {
-      print STDOUT " ($opts{'conf-tab'}, id $directive->{'id'})";
+    if ($opts->{'show-tags'}) {
+      print STDOUT " ($opts->{'conf-tab'}, id $directive->{id})";
     }
 
     print STDOUT "\n";
   }
 }
 
-# ---------------------------------------------------------------------------
 sub usage {
 
   print STDOUT <<END_OF_USAGE;
@@ -227,9 +226,9 @@ usage: $program [options]
 
  Table Options:
 
-  --conf-tab                   Default: $opts{'conf-tab'}
-  --ctxt-tab                   Default: $opts{'ctxt-tab'}
-  --map-tab                    Default: $opts{'map-tab'}
+  --conf-tab                   Default: $opts->{'conf-tab'}
+  --ctx-tab                    Default: $opts->{'ctx-tab'}
+  --map-tab                    Default: $opts->{'map-tab'}
 
  General Options:
 
