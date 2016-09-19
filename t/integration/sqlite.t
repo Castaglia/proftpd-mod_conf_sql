85dd38b0ce200e2393baeb07beb653a9ec3aedb7
TJ Saunders <tj@castaglia.org>
2016-09-19 11:42:09 -0700

    Development proceeding on the integration tests.

--- t/integration/sqlite.t
+++ t/integration/sqlite.t
@@ -2,6 +2,21 @@
 
 use strict;
 
+use File::Path qw(mkpath rmtree);
+use File::Spec;
 use Test::Simple tests => 1;
 
+my $tmpdir = $ARGV[0];
+print STDOUT "# tmpdir: $tmpdir\n";
+
+my $test_dir = (File::Spec->splitpath(abs_path(__FILE__)))[1];
+my $script = File::Spec->catfile($test_dir, '..', '..', 'sqlite-conf.sql');
+$script = File::Spec->canonpath($script);
+print STDOUT "# script: $script\n";
+
 ok(1 + 1 == 2);
+
+# Find sqlite-conf.sql from current directory.
+# Run sqlite3 exec to create "$tmpdir/proftpd.db"
+# Find proftpd binary, run "proftpd -td10 -c $url"
+# Cleanup "$tmpdir/proftpd.db"
--- t/integration/tests.pl
+++ t/integration/tests.pl
@@ -3,15 +3,20 @@
 use strict;
 
 use Cwd qw(abs_path);
+use File::Path qw(mkpath rmtree);
 use File::Spec;
+use File::Temp qw(tempdir);
 use Getopt::Long;
 use TAP::Harness;
+
+my $testno = 0;
 
 my $opts = {};
 GetOptions($opts, 'h|help', 'V|verbose');
 
 usage() if $opts->{h};
 $ENV{TEST_VERBOSE} = 1 if $opts->{V};
+$opts->{V} = 1 if $ENV{TEST_VERBOSE};
 
 my $test_dir = (File::Spec->splitpath(abs_path(__FILE__)))[1];
 
@@ -19,10 +24,13 @@ my $test_dir = (File::Spec->splitpath(abs_path(__FILE__)))[1];
 # being used to run test files other than those that ship with proftpd,
 # e.g. to run the tests that come with third-party modules.
 unless (defined($ENV{PROFTPD_TEST_BIN})) {
-  $ENV{PROFTPD_TEST_BIN} = File::Spec->catfile($test_dir, '..', 'proftpd');
+  my $bin = File::Spec->catfile($test_dir, '..', '..', 'proftpd');
+  $ENV{PROFTPD_TEST_BIN} = File::Spec->canonpath($bin);
 }
 
 $| = 1;
+
+print STDOUT "# PROFTPD_TEST_BIN = $ENV{PROFTPD_TEST_BIN}\n";
 
 my $test_files;
 if (scalar(@ARGV) > 0) {
@@ -30,12 +38,18 @@ if (scalar(@ARGV) > 0) {
 
 } else {
   $test_files = [
-    "$test_dir/sqlite.t"
+    ["$test_dir/sqlite.t", 'sqlite'],
   ];
 }
 
+# Create a temp directory for each separate test, pass it in, cleanup afterward
+my $tap_test_args = {
+  'sqlite' => [get_tmp_dir()],
+};
+
 my $tap_opts = {
   verbosity => 0,
+  test_args => $tap_test_args,
 };
 
 if ($opts->{V}) {
@@ -45,7 +59,29 @@ if ($opts->{V}) {
 my $harness = TAP::Harness->new($tap_opts);
 $harness->runtests(@$test_files) if scalar(@$test_files) > 0;
 
+# Cleanup
+foreach my $alias (keys(%$tap_test_args)) {
+  my $tmpdir = $tap_test_args->{$alias}->[0];
+  rmtree($tmpdir);
+}
+
 exit 0;
+
+sub get_tmp_dir {
+  ++$testno;
+
+  my $tmpdir = tempdir(
+    "mod_conf_sql-integration-$$-test$testno-XXXXXXXXXX",
+    TMPDIR => 1,
+    CLEANUP => 0,
+  );
+
+  unless (chmod(0755, $tmpdir)) {
+    croak("Can't chmod tmp directory $tmpdir: $!");
+  }
+
+  return $tmpdir;
+}
 
 sub usage {
   print STDOUT <<EOH;
