01774676001ce206e6f7a064648ca173927862b6
TJ Saunders <tj@castaglia.org>
2026-06-10 14:31:57 -0700

    Add MySQL-specific integration tests to the GitHub workflow.

--- t/integration/tests.pl
+++ t/integration/tests.pl
@@ -34,13 +34,11 @@ unless (defined($ENV{PROFTPD_TEST_BIN})) {
 $| = 1;
 
 my $test_files = [
-  ["$test_dir/mysql.t", 'mysql'],
   ["$test_dir/postgres.t", 'postgres'],
 ];
 
 # Create a temp directory for each separate test, pass it in, cleanup afterward
 my $tap_test_args = {
-  'mysql' => [get_tmp_dir()],
   'postgres' => [get_tmp_dir()],
 };
 
--- tests.pl
+++ tests.pl
@@ -62,6 +62,10 @@ if (scalar(@ARGV) > 0) {
   my $order = 0;
 
   my $FEATURE_TESTS = {
+    't/modules/mod_conf_sql/mysql.t' => {
+      order => ++$order,
+      test_class => [qw(mod_sql_mysql)],
+    },
   };
 
   my @feature_tests = testsuite_get_runnable_tests($FEATURE_TESTS);
